e6bca82d6f18b605771ca18aad54cc49bbfe2275
lemon_lyue <lemon_lyue@163.com>
2020-07-24 11:40:10 +0800

    update: 入口文件全局变量

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
 <?php
+// 全局变量
+require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'library'.DIRECTORY_SEPARATOR.'System.php';
 
+// composer 自动加载
 require __DIR__ . '/../vendor/autoload.php';
